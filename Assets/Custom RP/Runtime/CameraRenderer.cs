2008f13404be7fb0b2c39c42ca3556e41833c82f
FlyLing <963452757@qq.com>
2020-12-10 23:49:00 +0800

    SRP Study

---
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+    const string bufferName = "Render Camera";
+
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    ScriptableRenderContext context;
+    CullingResults cullingResults;
+
+    static ShaderTagId unlitShaderTagID = new ShaderTagId("SRPDefaultUnlit");
+
+    Camera camera;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeomertry();
+        Submit();
+    }
+
+    bool Cull()
+    {
+        ScriptableCullingParameters p;
+        if(camera.TryGetCullingParameters(out p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        //BeginSample 与 EndSample成对使用，为了在FrameDebugger中有层级显示
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
+    }
+
+    void DrawVisibleGeomertry()
+    {
+        var sortingSettings = new SortingSettings(camera);
+        var drawingSettings = new DrawingSettings(unlitShaderTagID,sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        context.DrawSkybox(camera);
+    }
+
+    void Submit()
+    {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+}
---
+++ Assets/Custom RP/Runtime/CustomRenderpipelineAsset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName ="Rendering/Custom Render Pipeline")]
+public class CustomRenderpipelineAsset : RenderPipelineAsset
+{
+
+
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new CustomRenderPipeline();
+    }
+
+}
